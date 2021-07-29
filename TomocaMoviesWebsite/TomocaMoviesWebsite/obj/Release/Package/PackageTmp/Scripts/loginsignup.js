ffb2fa2e861e40f08d048b015fdaa01fb95495be
nhoxboy9x42 <darkroller210@gmail.com>
2021-07-29 15:30:34 +0700

    Update Controler

--- TomocaMoviesWebsite/TomocaMoviesWebsite/obj/Release/Package/PackageTmp/Scripts/loginsignup.js
+++
@@ -1,7 +0,0 @@
-﻿var modal = document.getElementById('id01');
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
--- TomocaMoviesWebsite/TomocaMoviesWebsite/obj/Release/Package/PackageTmp/Scripts/triettren.js
+++
@@ -1,8 +0,0 @@
-﻿var counter = 1;
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 5) {
-        counter = 1;
-    }
-}, 7000);
